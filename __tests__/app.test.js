03e239ed5f08a118fa6b115ac804d52b826001bf
‘Nathan <ncash13@hotmail.com>
2018-08-21 15:20:56 -0500

    stuff from demo

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,31 @@
 
 const request = require('supertest');
 
-const server = require('../src/app');
+const app = require('../src/app');
 
 describe('app', () => {
+  it('responds with 404 for unknown path', ()=>{
+    return request(app)
+      .get('/404')
+      .expect(404)
+      .expect('Content-Type', 'text/html')
+      .expect('Resource Not Found');
+  });
+
+  it('responds with HTML for /', ()=>{
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html')
+      .expect(response =>{
+        expect(response.text[0]).toBe('<');
+      });
+  });
+  it('responds with 500 for /500', ()=>{
+    return request(app)
+      .post('/500')
+      .expect(500)
+      .expect('Content-Type', 'text/html')
+      .expect('Test Error');
+  });
 });
