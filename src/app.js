67dc48d11e7182f706b3e5e36126391b727f9ee8
‘Nathan <ncash13@hotmail.com>
2018-08-21 12:00:07 -0500

    listening to http

--- src/app.js
+++ src/app.js
@@ -3,3 +3,22 @@
 const http = require('http');
 
 const requestParser = require('./lib/request-parser');
+
+const app = http.createServer(requestHandler);
+module.exports = app;
+
+app.start = (port) =>{
+  return new Promise((resolve, reject)=>{
+    app.listen(port,(err,result)=>{
+      if(err){
+        reject(err);
+      } else{
+        resolve(result);
+      }
+    });
+  });
+};
+
+function requestHandler(req,res){
+
+}
